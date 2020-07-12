e5b643b09de0bb917f53e8c9ea723babe0e8bdc0
Yagna <yagna.b@git.assessmentcentre.uk>
2020-07-12 19:08:32 +0100

    started switching away from youtube-dl

--- CreatePlaylist.py
+++ CreatePlaylist.py
@@ -8,13 +8,13 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-import youtube_dl
+from youtube_title_parse import get_artist_title
 
 class CreatePlaylist:
     def __init__(self):
         self.user_id = user_id
         # self.token = self.get_spotify_token()
-        self.token = "BQDK99OHx4Mq6z_UqblJGl5u8255m2KwswFwnwpSojycVNX0FCVRyTw4WqUE9J8Wpq8IsSnp0jStBWpNLqSfZKnVUcIL2CqgMIHXIsJceIg1GPLssqXXIL4k6InE2nj15dsuNyZBfvVaCCU4JykxtaTExjBB4z80MLvikbq505ogf6VMXIOPL6rekMzSE_BH3qxtqSve3g"
+        self.token = "BQC_z1z_kUDXkefPn2DS9TmGXetuQMmKIAfHIHbEX9SIC54Re_4czx8_nR8ZKSSc3PtfW5YVLvM30C7G-ECNA2CndPif7YxfR6j4Fg7e9QAcHQEZoAS3x1mYSQnLPfBBStgE775GRlVwAtGYpIF1DGbtoxxEAiU7Okd_A8tnXgFURSDCVLN1hF21n3s9BSB2KOzWAFvu5Q"
         self.youtube_client = self.get_youtube_api()
         self.liked_songs_info = {}
 
@@ -53,58 +53,20 @@ class CreatePlaylist:
         )
         response = request.execute()
 
-        print("Length of liked videos: {}".format(len(response['items'])))
 
-        # only using youtube-dl for first video which worked on example repo
-        item = response['items'][0]
-        video_title = item["snippet"]["title"]
-        youtube_url = "https://www.youtube.com/watch?v={}".format(item['contentDetails']['videoId'])
-        video = youtube_dl.YoutubeDL({}).extract_info(youtube_url, False)
-        song_name = video['track']
-        artist = video['artist']
-        print("Youtube url: {0}"
-              "\nTitle: {5}"
-              "Item object: "
-              "\n{1}"
-              "\nSong name{2}"
-              "\nArtist: {3}"
-              "\nYT-DL object:"
-              "\n{4}".format(youtube_url, item, song_name, artist, video, video_title))
 
-        # for item in response['items']:
-        #     video_title = item["snippet"]["title"]
-        #     youtube_url = "https://www.youtube.com/watch?v={}".format(item['contentDetails']['videoId'])
-        #
-        #     try:
-        #         # use youtube to parse song information
-        #         video = youtube_dl.YoutubeDL({}).extract_info(youtube_url, False)
-        #
-        #         if 'track' in video:
-        #             print("track attribute found for {}".format(youtube_url))
-        #         if 'artist' in video:
-        #             print("video attribute found for {}".format(youtube_url))
-        #
-        #         song_name = video['track']
-        #         artist = video['artist']
-        #
-        #         print("Information for {0}\nSong title: {1}\nArtist: {2}\n".format(
-        #             video_title, song_name, artist))
-        #
-        #         if song_name is not None and artist is not None:
-        #             print("SONG NAME: {0}, ARTIST: {1} PASSED IF CHECK".format(song_name, artist))
-        #
-        #             # save information
-        #             self.liked_songs_info[video_title] = {
-        #                 "youtube_url": youtube_url,
-        #                 "song_name": song_name,
-        #                 "artist": artist,
-        #
-        #                 # spotify resource uri for easy access
-        #                 "spotify_uri": self.get_song_spotify_uri(song_name, artist)
-        #             }
-        #     except:
-        #         print("{} can no longer be accessed.".format(youtube_url))
+        for item in response['items']:
+            video_title = item["snippet"]["title"]
+            youtube_url = "https://www.youtube.com/watch?v={}".format(item['contentDetails']['videoId'])
 
+            self.liked_songs_info[video_title] = {
+                "youtube_url": youtube_url,
+                "song_name": song_name,
+                "artist": artist,
+
+                # spotify resource uri for easy access
+                "spotify_uri": self.get_song_spotify_uri(song_name, artist)
+            }
 
     # Create corresponding playlist on Spotify
     def create_playlist(self):
@@ -196,5 +158,6 @@ class CreatePlaylist:
 if __name__ == '__main__':
     cp = CreatePlaylist()
     # print(cp.add_songs_to_playlist())
-    cp.get_liked_videos()
-    print(cp.liked_songs_info)
+    # cp.get_liked_videos()
+    # print(cp.liked_songs_info)
+    print(cp.get_spotify_token())
