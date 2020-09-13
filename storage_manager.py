c9945cc01d53f4d2ae4b4d53a9d5e9887c130b49
Yagna <yag_bork@outlook.co.uk>
2020-09-13 20:17:07 +0100

    created db seed function (init_pickle_file), db file path is now ./db_songs

--- storage_manager.py
+++ storage_manager.py
@@ -10,12 +10,12 @@ from datetime_manager import datetime_to_timestamp_str, timestamp_str_to_datetim
 
 class StorageManager:
     def __init__(self):
-        self.store_path = './downloaded_songs_store'
+        self.db_file_path = 'songs_db'
 
     def read_storage(self):
         db = {}
-        if os.path.getsize(self.store_path) > 0:
-            with open(self.store_path, 'rb') as store:
+        if os.path.getsize(self.db_file_path) > 0:
+            with open(self.db_file_path, 'rb') as store:
                 db = pickle.load(store)
 
         return db
@@ -63,8 +63,8 @@ class StorageManager:
 
     def get_db(self):
         db = {}
-        if os.path.getsize(self.store_path) > 0:
-            with open(self.store_path, 'rb') as store:
+        if os.path.getsize(self.db_file_path) > 0:
+            with open(self.db_file_path, 'rb') as store:
                 db = pickle.load(store)
 
         return db
@@ -91,7 +91,40 @@ class StorageManager:
 
             pickle.dump(db, store)
 
+    @staticmethod
+    def init_pickle_file(file):
+        init_obj = {
+            "synced_playlists": [{
+                "PLucKeiEo64s_2ZXtW0Vv3kJ8rqFEvDJbf": {
+                    "spotify_id": "4dVm4zXMIU3HqjbVtRrtRV",
+                    "last_synced": 1599412611.302752,
+                    "is_downloaded": False,
+                }, "LLEA6rXRPPbQur1xyOgurtQg": {
+                    "spotify_id": "4EOYcqOkQMOPB8bWmzZc3U",
+                    "last_synced": 1599412631.782011,
+                    "is_downloaded": False,
+                }, "PLucKeiEo64s9D4vtua7xIFZe-9YZdOmZP": {
+                    "spotify_id": "5vAwYEmFbF2yRex2p7fwYB",
+                    "last_synced": 1594657869.692405,
+                    "is_downloaded": False,
+                }, "PLucKeiEo64s9376jBeUh9ukrWv6L5k0Ox": {
+                    "spotify_id": "2B46Xhd7rw6IRYWkcAGDO4",
+                    "last_synced": 1599412631.888807,
+                    "is_downloaded": False,
+                }, "PLucKeiEo64s8tejNdkXCKo5xClilZ4Nni": {
+                    "spotify_id": "4C5kAlMObPKLDRwsdkDg9y",
+                    "last_synced": 1594657869.171505,
+                    "is_downloaded": True
+                }
+            }],
+            "slowed_songs": {},
+        }
+
+        with open(file, 'wb') as store:
+            pickle.dump(init_obj, store)
+
+
 if __name__ == '__main__':
     storage = StorageManager()
-    print(storage.get_last_synced_timestamp('PLucKeiEo64s_2ZXtW0Vv3kJ8rqFEvDJbf'))
-    print(storage.get_spotify_playlist_id('PLucKeiEo64s_2ZXtW0Vv3kJ8rqFEvDJbf'))
+    storage.init_pickle_file(storage.db_file_path)
+    print(storage.read_storage())
